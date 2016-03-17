3c891276142f25898f5453826509e516d621dcc7
Kami Boers <kamiboers@gmail.com>
2016-03-16 20:19:04 -0600

    I4: game limits guesses and resets with wins

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,54 +1,60 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+@@SECRET_NUMBER = rand(100)
 @@remaining_guesses = 5
 
 get '/' do
-  @@remaining_guesses -= 1
-  guess = params["guess"]
-  message = check_guess(guess)
+    guess = params["guess"]
+    message = check_guess(guess)
   erb :index, :locals => {:number => @@SECRET_NUMBER,
     :message => message,
     :background => @background}
   end
 
-  def check_guess(guess)
 
-    if guess.nil?
+
+  def check_guess(guess)
+    if @@remaining_guesses == 0
+      @@remaining_guesses = 5
+      @@SECRET_NUMBER = rand(100)
+        message = "You've taken too many guesses! A new number has been generated."
+    elsif guess.nil?
       message = ""
 
     else
 
       guess = guess.to_i
+      @@remaining_guesses -= 1
 
-      if guess > SECRET_NUMBER
+      if guess > @@SECRET_NUMBER
 
-        if (SECRET_NUMBER-guess).abs <= 5
+        if (@@SECRET_NUMBER-guess).abs <= 5
           @background = "#F5A9A9"
           message = "Too high!"
-        elsif  (SECRET_NUMBER-guess).abs > 5
+        elsif  (@@SECRET_NUMBER-guess).abs > 5
           @background = "#FF0000"
           message = "Way too high!"
         end
 
-      elsif guess < SECRET_NUMBER
+      elsif guess < @@SECRET_NUMBER
 
-        if (SECRET_NUMBER-guess).abs <= 5
+        if (@@SECRET_NUMBER-guess).abs <= 5
           @background= "#F5A9A9"
           message = "Too low!"
-        elsif  (SECRET_NUMBER-guess).abs > 5
+        elsif  (@@SECRET_NUMBER-guess).abs > 5
           @background = "#FF0000"
           message = "Way too low"
         end
 
-      elsif guess == SECRET_NUMBER
+      elsif guess == @@SECRET_NUMBER
         @background = "#74DF00"
-        message = "You got it right!"
+        @@remaining_guesses = 5
+        @@SECRET_NUMBER = rand(100)
+        message = "You got it right! A new number has been generated."
       end
 
     end
-
 
 
   end
